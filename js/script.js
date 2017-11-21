bb060ec80cb8d7c281f590a61afc0eb0d287ad2b
Brad Klaver <bklaver@echidna.ca>
2017-11-21 13:52:03 -0500

    alt tags on project overview images

--- js/script.js
+++ js/script.js
@@ -30,8 +30,16 @@
       });
     }
   };
+  Drupal.behaviors.de_theme_cwrc_featured_projects = {
+    attach: function (context, settings) {
+      jQuery('.view-id-project_overview .view-mode-project_overview_grid_item').each(function(index, e)
+      {
+        jQuery(e).find('img').attr('alt', jQuery(e).find('.field-name-body .field-item').text());
+      });
+    }
+  };
 
-  Drupal.behaviors.de_theme_cwrc_homepage_slider = {
+    Drupal.behaviors.de_theme_cwrc_homepage_slider = {
     attach: function (context, settings) {
       "use strict";
 
